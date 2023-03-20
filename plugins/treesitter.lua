32d1a26f856a4338401c2ac20644277fb2d94c80
Shantanu Shukla <shantanu.shukla10@gmail.com>
2023-03-20 13:10:50 +0530

    add auto_install for parsers in treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -4,13 +4,10 @@ return {
     "mrjones2014/nvim-ts-rainbow",
   },
   opts = {
+    auto_install = vim.fn.executable "tree-sitter" == 1,
     highlight = { disable = { "help" } },
     indent = { enable = true, disable = { "python" } },
     matchup = { enable = true },
     rainbow = { enable = true },
-    ensure_installed = {
-      "lua",
-      "python",
-    }
   },
 }
