1e948eafd870b6232d2403d4ca9dc6759a5568aa
Shantanu Shukla <shantanu.shukla10@gmail.com>
2023-07-02 16:59:20 +0530

    refactor add new editor plugins

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -2,6 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      hijack_netrw_behavior = "open_default",
       filtered_items = {
         visible = true -- show hidden/ignored files with different color
       }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,15 @@
 return {
   { "simrat39/rust-tools.nvim" },
   {
+    "ethanholz/nvim-lastplace",
+    event = "User AstroFile",
+    opts = {
+      lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+      lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+      lastplace_open_folds = true
+    }
+  },
+  {
     "lambdalisue/suda.vim",
     event = "User AstroFile",
     enabled = function() return not vim.g.win32 end
@@ -14,5 +23,24 @@ return {
       { "<M-h>", mode = { "n", "v" } },
     },
     opts = {},
+  },
+  {
+    "arsham/indent-tools.nvim",
+    dependencies = { "arsham/arshlib.nvim" },
+    event = "User AstroFile",
+    config = function() require("indent-tools").config {} end,
+  },
+  {
+    "echasnovski/mini.surround",
+    keys = {
+      { "sa", desc = "Add surrounding", mode = { "n", "v" } },
+      { "sd", desc = "Delete surrounding" },
+      { "sf", desc = "Find right surrounding" },
+      { "sF", desc = "Find left surrounding" },
+      { "sh", desc = "Highlight surrounding" },
+      { "sr", desc = "Replace surrounding" },
+      { "sn", desc = "Update `MiniSurround.config.n_lines`" },
+    },
+    opts = { n_lines = 200 },
   },
 }
