750dfbcf9ae1ebe40a0aaf28aa93c081c7bad263
Shantanu Shukla <shantanu.shukla10@gmail.com>
2023-07-02 16:11:51 +0530

    plugins: move from nvim-gomove to mini.move

--- plugins/user.lua
+++ plugins/user.lua
@@ -5,4 +5,14 @@ return {
     event = "User AstroFile",
     enabled = function() return not vim.g.win32 end
   },
+  {
+    "echasnovski/mini.move",
+    keys = {
+      { "<M-l>", mode = { "n", "v" } },
+      { "<M-k>", mode = { "n", "v" } },
+      { "<M-j>", mode = { "n", "v" } },
+      { "<M-h>", mode = { "n", "v" } },
+    },
+    opts = {},
+  },
 }
