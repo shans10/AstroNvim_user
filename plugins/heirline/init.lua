21a691577105ad6d336e86e7f726d8ab6bb0f517
Shantanu Shukla <shantanu.shukla10@gmail.com>
2023-09-03 12:15:43 +0530

    heirline: remove default theme with filename

--- plugins/heirline/init.lua
+++ plugins/heirline/init.lua
@@ -11,15 +11,10 @@ return {
     -- Set statusline based on chosen theme
     if themes[theme] then
       opts.statusline = require("user.plugins.heirline.themes." .. theme)
-      -- If tabline is disabled show default statusline with filename
-    elseif vim.o.stal == 0 and not vim.g.winbar_enabled then
-      opts.statusline = require "user.plugins.heirline.themes.default"
     end
 
     -- Override winbar configuration
-    if theme == "lunarvim" and vim.o.stal == 0 then
-      opts.winbar = require "user.plugins.heirline.winbar"
-    elseif theme == nil and vim.o.stal == 0 and vim.g.winbar_enabled then
+    if (theme == "lunarvim" or theme == nil) and vim.o.stal == 0 then
       opts.winbar = require "user.plugins.heirline.winbar"
     elseif not vim.g.winbar_enabled then
       opts.winbar = nil
